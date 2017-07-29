17541ef7a267cace0ca5afe510b58c5299fd6f5a
JacksonToomey <jacksontoomey@gmail.com>
2017-07-29 08:28:05 -0400

    sort todos

--- store/state/todos/selectors.js
+++ store/state/todos/selectors.js
@@ -4,5 +4,17 @@ const getTodos = ({ todos }) => todos;
 
 export const getSortedTodos = createSelector(
     [getTodos],
-    todos => todos.toList()
+    todos => todos.toList().sort((f, s) => {
+        if(f.get('complete') && !s.get('complete')) {
+            return 1;
+        }
+        if(s.get('complete') && !s.get('complete')) {
+            return -1;
+        }
+
+        if(f.get('id') > s.get('id')) {
+            return -1;
+        }
+        return 0;
+    })
 )
